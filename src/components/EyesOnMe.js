36ce2f58ffb3d2ec055b3a58d653b7b1df5f22b5
Lyn Finman <lynfinman@gmail.com>
2022-06-08 09:29:45 -0400

    commit tree d3457dabc429c7fa4312c4094678359835642c27

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+
+function handleOnFocus(event) {
+    console.log('Good!')
+}
+
+function handleOnBlur(event) {
+    console.log('Hey! Eyes on me!')
+}
+
+function EyesOnMe() {
+    return (
+        <button onFocus={handleOnFocus} onBlur={handleOnBlur}>Eyes on me</button>
+    )
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
-// Code Keypad Component Here
+import React, { useState } from 'react';
+
+function Keypad() {
+    const [password, setPassword] = useState('');
+
+    function handlePasswordInput(event) {
+        console.log('Entering password...');
+    }
+
+    return (
+        <input onChange={handlePasswordInput} type="password" />
+    )
+}
+export default Keypad;
